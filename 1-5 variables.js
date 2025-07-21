675ed7039db9543db7cf87e6697b3c7ca2e725a4
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2025-07-21 12:15:04 +0900

    Add files via upload

---
+++ 1-5 variables.js
@@ -0,0 +1,35 @@
+let message = 'Hello';
+alert(message);
+
+// 값 변경
+message = 'World!';
+alert(message);
+
+// $와 _만을 이용한 변수명
+let $ = 1; 
+let _ = 2; 
+
+alert($ + _); // 3
+
+// 상수
+const myBirthday = '18.04.1982';
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+// 색상을 고르고 싶을 때 별칭을 사용할 수 있게 되었습니다.
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
+// 과제 1
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
+
+// 과제 2
+let ourPlanetName;
+let currentUserName;
+
+// 과제 3
